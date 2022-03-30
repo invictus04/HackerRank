096f5eb0b8dbc8610c9657530523d97f34323ad5
invictus04 <97658238+invictus04@users.noreply.github.com>
2022-03-30 19:02:44 +0530

    Create javaStaticinitializer.java

---
+++ javaStaticinitializer.java
@@ -0,0 +1,28 @@
+rt java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+static boolean flag = true;
+static int B,H;
+static {
+    Scanner sc = new Scanner(System.in);
+     B = sc.nextInt();
+     H = sc.nextInt();
+    if(B<=0 || H<=0) {
+        flag = false;
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    } 
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
