d0c1f8aa14506cf6a50a97b50825e04be635a9ff
invictus04 <97658238+invictus04@users.noreply.github.com>
2022-03-30 18:59:36 +0530

    Create Javaloops2.java

---
+++ Javaloops2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math.*;
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int chintu = a;
+            for(int j = 0; j < n; j++) {
+                chintu += (Math.pow(2,j) * b);
+                System.out.print(chintu + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
